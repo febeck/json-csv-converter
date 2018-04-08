1447bd688ea9d06ec8b4258265b1c6e8bdf9f840
Fernando Beck <fe.beck25@gmail.com>
2018-04-08 13:13:53 +0200

    [Allow getting user properties and values and test all existing methods

--- main.rb
+++ main.rb
@@ -1,6 +1,5 @@
 require 'json'
 
-
 def get_users(file_path)
     begin
         users_file = File.read(file_path)
@@ -14,6 +13,50 @@ def get_users(file_path)
     end
 end
 
+def push_simple_property(base_array, property)
+    base_array << property
+end
 
+def push_array(base_array, array)
+    base_array << array.join(',')
+end
+
+def is_object?(variable)
+    variable.is_a?(Hash)
+end
+
+def get_properties(user, properties=[], base_key=nil)
+    user.keys.each do |key|
+        if is_object?(user[key])
+            if base_key
+                get_properties(user[key], properties, "#{base_key}.#{key}")
+            else
+                get_properties(user[key], properties, key)
+            end
+        else
+            if base_key
+                properties << "#{base_key}.#{key}"
+            else
+                properties << key
+            end
+        end
+    end
+    return properties
+end
+
+def get_values(user, values=[])
+    user.keys.each do |key|
+        if is_object?(user[key])
+            get_values(user[key], values)
+        else
+            if user[key].is_a?(Array)
+                push_array(values, user[key])
+            else
+                push_simple_property(values, user[key])
+            end
+        end
+    end
+    return values
+end
 
 users = get_users('./users.json')
---
+++ test.rb
@@ -0,0 +1,61 @@
+require 'json'
+require "test/unit"
+
+require_relative "main.rb"
+ 
+class TestUsersConverter < Test::Unit::TestCase
+    def test_get_users_successful
+        users_file = File.read('./users.json')
+        users_hash = JSON.parse(users_file)
+        assert_equal(users_hash, get_users('./users.json') )
+    end
+
+    def test_push_simple_property
+        base_array = [1]
+        property = 2
+        assert_equal([1, 2], push_simple_property(base_array, property))
+    end
+
+    def test_push_array
+        base_array = [1]
+        property = ["a", "b"]
+        assert_equal([1, "a,b"], push_array(base_array, property))
+    end
+
+    def test_is_object
+        assert_equal(false, is_object?(1))
+        assert_equal(false, is_object?('abc'))
+        assert_equal(false, is_object?([1, 2]))
+        assert_equal(true, is_object?({'a' => 'b'}))
+    end
+
+    def test_get_properties
+        object = {
+            'a' => 'value a',
+            'b' => {
+                'b1' => {
+                    'b12' => 'value b12'
+                },
+                'b2' => {
+                    'b22' => 'value b22'
+                }
+            }
+        }
+        assert_equal(['a', 'b.b1.b12', 'b.b2.b22'], get_properties(object))
+    end
+
+    def test_get_values
+        object = {
+            'a' => 'value a',
+            'b' => {
+                'b1' => {
+                    'b12' => 'value b12'
+                },
+                'b2' => {
+                    'b22' => 'value b22'
+                }
+            }
+        }
+        assert_equal(['value a', 'value b12', 'value b22'], get_values(object))
+    end
+end
